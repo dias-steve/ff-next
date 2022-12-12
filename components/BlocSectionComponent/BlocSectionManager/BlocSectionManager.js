b159b81bf9c203582ce81045028b7c6aa6026d4e
Steve Dias <stevediaspro@gmail.com>
2022-12-12 11:57:05 +0100

    [FIX][HEADER / FOOTER]

--- components/BlocSectionComponent/BlocSectionManager/BlocSectionManager.js
+++ components/BlocSectionComponent/BlocSectionManager/BlocSectionManager.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import BlocSection from '../BlocSection/BlocSection'
 import styles from './BlocSectionManager.module.scss'
-
+import { v4 as uuidv4 } from 'uuid';
 
 export default function BlocSectionManager({content}) {
   return (
     <>{ content && Array.isArray(content) && content.length > 0 &&
         <>
         {content.map(bloc => {
-            return <BlocSection data={bloc}/>
+            return <BlocSection data={bloc} key={uuidv4()}/>
         })}
         </>
     }
